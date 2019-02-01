dcbf3b09726707543e60be5a4516e1656c8113de
Richard Andrew Flores <richaraf@usc.edu>
2019-02-01 03:31:56 -0800

    Update Main.cpp

--- pong/Main.cpp
+++ pong/Main.cpp
@@ -1,16 +1,6 @@
-//
-//  Main.cpp
-//  Game-mac
-//
-//  Created by Sanjay Madhav on 5/31/17.
-//  Copyright © 2017 Sanjay Madhav. All rights reserved.
-//
-
 #include "Game.h"
 
-int main(int argc, char** argv)
-{
-	// TODO
+int main(int argc, char** argv) {
 	Game* Pong = new Game;
 
 	if (!Pong->initialize())
